5881c6053dba8013e00e4128e440cf3d9d81e722
Hp <rs739025@gmail.com>
2025-11-03 03:47:15 +0530

    add the wallet adapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import './App.css'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
 import { Tokenlaunchpad } from './components/Tokenlauchpad'
 
 function App() {
   return (
-    <Tokenlaunchpad></Tokenlaunchpad>
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              padding: 20
+            }}>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
+            </div>
+            <Tokenlaunchpad />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   )
 }
 
